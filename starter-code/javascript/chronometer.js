28a83a183e87d201fdd5fb077931cc3665be36cb
Konstantin <dauer.konstantin@gmail.com>
2020-01-15 18:02:44 +0100

    tests done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,39 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0,
+    this.intervalId
+  }
+
+
+  startClick() {
+   this.intervalId = setInterval(() => {
+
+      this.currentTime +=1;
+    }, 1000)
+  }
+
+  getMinutes() {
+      return Math.floor(this.currentTime / 60)
+  }
+
+  getSeconds() {
+      return this.currentTime % 60;      
+  }
+  
+  twoDigitsNumber(time) {
+    if (time < 9) {
+      return `0${time}`
+    }
+    else if (time >= 10)
+    return time.toString();
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
+  splitClick() {}
 }
